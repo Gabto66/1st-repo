4b895a86d3f5f1345dae029e373c18d82cd4a208
Gabto66 <98637095+Gabto66@users.noreply.github.com>
2022-01-29 09:49:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 
-namespace _27._11._21
+namespace _13._11
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double MoniinUSD = double.Parse(Console.ReadLine());
-            double BTCpriceinUSD = double.Parse(Console.ReadLine());
-            double SatoshionByte = double.Parse(Console.ReadLine());
-            double BTCavailabletoBuy = MoniinUSD / BTCpriceinUSD;
-            double TotalTransactionCostinBTC = BTCavailabletoBuy *(SatoshionByte * 1024) / 100000000;
-            double BTCbought = BTCavailabletoBuy - TotalTransactionCostinBTC;
-            double USDspendOnTax = TotalTransactionCostinBTC * BTCpriceinUSD;
-            double progamerCom = BTCbought * (10.0 / 100);
-            double BTCafterExp = BTCbought - progamerCom;
-            Console.WriteLine($"Total BTC after exp:{BTCafterExp:F5}BTC");
-            Console.WriteLine($"Tax:{USDspendOnTax:F2}USD");
-            Console.WriteLine($"Programmer`s payment: {progamerCom:F5} BTC");
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
